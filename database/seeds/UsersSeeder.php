59ff1fda98d7894c8856c002381f80b68593b234
rusla <suhrugen@gmail.com>
2020-08-24 01:59:05 +0700

    добавлено поле is_admin в users и два пользователя

---
+++ database/seeds/UsersSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'Администратор',
+                'email' => 'admin@admin.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('321'),
+                'remember_token' => Str::random(10),
+                'is_admin' => true,
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+            [
+                'name' => 'Пользователь',
+                'email' => 'user@user.com',
+                'email_verified_at' => now(),
+                'password' => Hash::make('123'),
+                'remember_token' => Str::random(10),
+                'is_admin' => false,
+                'created_at' => now(),
+                'updated_at' => now()
+            ],
+        ]);
+    }
+}
